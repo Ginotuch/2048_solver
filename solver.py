e7d676a5035a91f49a49008b777bbd28bb340f34
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2017-07-28 14:35:22 +1200

    Increased solver average score

--- solver.py
+++ solver.py
@@ -6,43 +6,50 @@ import game
 
 def main():
     game_map = game.initialise_map_blank(4)
-    database = [
+    database = [  # Example of what the database might look like
+        [{}, 0],  # game map
         {
             "mv": 1,
-            "dead": False,
+            "gover": False,
             "map": [{}, 0],
             "next": []},
 
         {
             "mv": 2,
-            "dead": False,
+            "gover": False,
+            "completed":False,
             "map": [{}, 0],
             "next": []}
     ]
-    history = [0]
+    previous_move = None
+    past_scores = []
     highest_score = 0
     games = 0
     count = 0
     while True:
-        if game_map == history[-1]:
+        if game_map == previous_move:
             direction = randrange(1, 5, 3)
         else:
             direction = randrange(2, 4)
         game.move(direction, game_map)
-        history += [deepcopy(game_map)]
-        # game.print_current(game_map) # Commented out as it slows down the code a huge amount
+        previous_move = deepcopy(game_map)
+        #game.print_current(game_map) # Commented out as it slows down the code a huge amount
         if not game.movement_check(game_map):
             count += 1
             if count % 20 == 0:
                 print("Games complete:", count)
             if game_map[1] > highest_score:
                 highest_score = game_map[1]
+            previous_move = deepcopy(game_map)
+            past_scores += [game_map[1]]
             game_map = game.initialise_map_blank(4)
             games += 1
             if games == 2000:
                 break
         game.new_num(game_map)
+    print("Average score:", sum(past_scores) / len(past_scores))
     print("Out of", games, "games, the highest score was:", highest_score)
+    input()
 
 
 main()
