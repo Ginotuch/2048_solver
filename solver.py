3710344d9cf6b9a048a15002e14b5bb2a587dc6f
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2017-07-31 20:16:23 +1200

    Added time/moves tracking

--- solver.py
+++ solver.py
@@ -1,6 +1,18 @@
-from copy import deepcopy
-from random import choice, randrange
+"""
+2048 solver - solver.py
+----
+This is the solver, there will be two versions, the basic solver which
+pretty much presses random buttons till it can't move anymore (although
+with a little extra stuff).
 
+Then there will be the Tree explorer, which will try to brute force the
+game and find the highest score it can.
+
+Author: Ginotuch
+"""
+
+from random import choice, randrange
+from time import time
 from game import GameMap
 
 
@@ -15,12 +27,15 @@ def basic_solver(game_map, retries):
     games = 0
     count = 0
     moved = False
+    start = time()
+    moves = 0
     while True:
         if not moved:
             direction = choice([1, 2, 3, 4])
         else:
             direction = choice([2, 3])
         moved = game_map.move(direction)
+        moves += 1
         #game_map.print_current(False)  # Commented out as it slows down the code a huge amount
         if not game_map.movement_check():
             count += 1
@@ -35,8 +50,20 @@ def basic_solver(game_map, retries):
                 break
         if moved:
             game_map.new_num()
+    end = time()
+    total_time = (end - start)
+    hours = int((total_time // 3600) // 60)
+    total_time = total_time % 3600
+    minutes = int(total_time // 60)
+    seconds = round(total_time % 60, 3)
+    milliseconds = str(round(seconds - int(seconds), 3))[2:]
+    seconds = int(seconds)
+    time_string = str(hours) + ":" + str(minutes) + ":" + str(seconds) + ":" + str(milliseconds)
+    print("Time taken:", time_string)
+    print("Total moves:", moves)
     print("Average score:", sum(past_scores) / len(past_scores))
     print("Out of", games, "games, the highest score was:", highest_score)
+    print("Lowest score:", min(past_scores))
 
 
 main()
